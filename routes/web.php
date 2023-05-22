8468c78fffca37204cf1976b88481df396541aec
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-22 14:33:59 +0200

    removed web.php guest

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Auth\RegisterController;
 }); */
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/login', LoginController::class);
     Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
     Route::post('/register', RegisterController::class)->middleware('guest');
 });
