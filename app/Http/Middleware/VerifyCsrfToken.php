e4ea6f42a105fde3d12bf95d71eac18ad5cbbda2
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-23 13:16:02 +0200

    xsrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,14 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "139.59.212.68",
+        "46.101.216.212"
     ];
+
+    protected function shouldPassThrough($request)
+    {
+        $ip = $request->ip();
+
+        return in_array($ip, $this->except);
+    }
 }
