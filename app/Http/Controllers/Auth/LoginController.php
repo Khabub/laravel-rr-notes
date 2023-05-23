008649c36e0788eb5dd575646406513214022de0
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-23 14:36:53 +0200

    xsrf token

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends Controller
     public function __invoke(LoginRequest $request)
     {
         // Verify CSRF token
-        $expectedToken = $request->session()->token(); // Retrieve the stored CSRF token value
+        /* $expectedToken = $request->session()->token(); // Retrieve the stored CSRF token value
 
         $receivedToken = $request->input('_token'); // Retrieve the CSRF token from the request payload
 
@@ -25,31 +25,32 @@ class LoginController extends Controller
             throw ValidationException::withMessages([
                 '_token' => ['CSRF token mismatch. Nesouhlasí.'],
             ]);
-        }
+        } */
 
-        /* $user = User::where('name', $request->name)->first();
+        $user = User::where('name', $request->name)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)){
             throw ValidationException::withMessages([
-                'name' => ['The credentials you entered are incorrect']
+                'input' => ['The credentials you entered are incorrect'],
+                'inputCze' => ['Zadané přihlašovací údaje jsou nesprávné']
             ]);
         }
 
         return response()->json([
             'user' => $user,
             'token' => $user->createToken('laravel_api_token')->plainTextToken
-        ]); */
+        ]);
 
 
 
 
         // použít při api-web
 
-        if (!auth()->attempt($request->only(['name', 'password']))) {
+        /* if (!auth()->attempt($request->only(['name', 'password']))) {
             throw ValidationException::withMessages([
                 'input' => ['The credentials you entered are incorrect'],
                 'inputCze' => ['Zadané přihlašovací údaje jsou nesprávné']
             ]);
-        }
+        } */
     }
 }
