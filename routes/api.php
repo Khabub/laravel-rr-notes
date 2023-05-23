c7d0e15a10265e36e603bb9c0accf714c8fa2238
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-23 15:02:08 +0200

    xsrf token

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NotesController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 
 /*
