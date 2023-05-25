e8819177a9aaff12e1a16cd8337d16c139cd322a
Robert Rozehnal <rob.kab.roz@gmail.com>
2023-05-25 18:51:13 +0200

    note fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Notes;
+use App\Models\Note;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::factory(5)->has(Notes::factory(5))->create();
+        User::factory(5)->has(Note::factory(5))->create();
 
 
         /* $this->call([
